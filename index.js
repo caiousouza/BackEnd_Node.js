a43b506b364bb36f34e88533ec2076c0bee44562
Caio Souza Queiroz <caiousouza113@gmail.com>
2022-08-17 13:38:29 -0400

    Listagem de EndPoints e criação dos EndPoints de leitura (Single and all)

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require('express')
 const app = express()
 
-
-app.get("/", function (req, res) {
+app.get("/",  (req, res) => {
     res.send("Hello, World!");
 });
 
-app.get("/oi", function (req, res) {
+app.get("/oi",  (req, res) => {
     res.send("Olá, mundo!");
 });
 
-app.listen(3000)
+/*
+Lista de EndPoints da aplicação CRUD de Mensagens.
+CRUD: create; Read (single & all); Update; Delete;
+- [GET] /Mensagens/{id} - Retorna apenas uma unica mensagens pelo id.
+- [POST] /Mensagens - Cria  uma nova mensagem.
+- [PUT] /Mensagens/{id} - Atualiza uma Mensagem pelo ID.
+- [DELETE] /Mensagens/{id} - Remove uma mensagem pelo ID.
+*/
+
+const Mensagens = [
+    "Essa é a preimeira Mensagem",
+    "Essa é a segunda Mensagem",
+    "Essa é a terceira Mensagem",
+    "Essa é a quarta Mensagem",
+    "Essa é a quinta Mensagem"
+];
+
+app.get('/Mensagens', (req, res) => {
+    res.send(Mensagens);
+})
+
+app.get('/Mensagens/:id', (req, res) => {
+    const id = req.params.id -1;
+    const mensagem = Mensagens [id];
+    res.send(mensagem);
+})
+
+app.listen(3000, function() {
+    console.info('App rodando em http://localhost:3000')
+});
